3843232c9cd083521cccaa5d9b96d0127acd7642
Reeny12 <lebotsamaureen@gmail.com>
2023-09-22 03:03:41 -0700

    Configuration management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# The code will create a file named school inside the /tmp directory.
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
